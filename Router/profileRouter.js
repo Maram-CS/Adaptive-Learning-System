365cd2b48df9f0522ff36e1b361afca437b64204
Maram BERRAHAL <maramberrahal5@gmail.com>
2026-03-07 21:57:01 +0100

    here i added frofile-view page and linked it with backend and profile page

--- Router/profileRouter.js
+++ Router/profileRouter.js
@@ -1,8 +1,7 @@
 import {Router} from "express";
-import {getProfile,createProfile} from "../Controller/profileController.js";
+import {createProfile} from "../Controller/profileController.js";
 import { upload } from "../ConfigDB/multerConfig.js";
 const profileRouter = Router();
 profileRouter.post("/create", upload.single("profilePicture"), createProfile);
-profileRouter.get("/profileInfo/:email", getProfile);
 
 export default profileRouter;
