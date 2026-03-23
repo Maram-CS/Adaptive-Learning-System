65e8a2828e77f04615e11f8546175269afc9f91e
Maram BERRAHAL <maramberrahal5@gmail.com>
2026-03-23 12:17:33 +0100

    here i fixed profile and home css

---
+++ Model/courseModel.js
@@ -0,0 +1,36 @@
+import { Schema,model } from "mongoose";
+
+const courseSchema = new Schema({
+    Title : {
+        type : String,
+        required : true,
+    },
+    Description : {
+        type : String,
+        required : true,
+    },
+    Instructor : {
+        type : Schema.Types.ObjectId,
+        ref : "userModel",
+        required : true,
+    },
+    category : {
+        type : String,
+        required : true,
+        enum : ["frontend", "backend", "database", "tools", "design", "deployment"],
+    },
+    image : {
+        type : String,
+        required : true,
+    },
+    rating : [
+        {
+            userId : string,
+            value : number,
+        }
+    ]
+
+},{timestamps : true});
+
+const courseModel = model("course",courseSchema);
+export default courseModel;
--- Router/profileRouter.js
+++ Router/profileRouter.js
@@ -1,10 +1,10 @@
 import {Router} from "express";
 import {createProfile,editProfile,viewProfile} from "../Controller/profileController.js";
-import authRequest from "../midalware/authMidalware.js";
+import authRequest from "../middleware/authMiddleware.js";
 import { upload } from "../ConfigDB/multerConfig.js";
 
 const profileRouter = Router();
-profileRouter.post("/create", upload.single("profilePicture"), createProfile);
-profileRouter.post("/edit", upload.single("profilePicture"), editProfile);
-profileRouter.get("/Profile-view",authRequest, viewProfile );
+profileRouter.post("/create",authRequest, upload.single("profilePicture"), createProfile);
+profileRouter.post("/edit", authRequest, upload.single("profilePicture"), editProfile);
+profileRouter.get("/Profile-view", authRequest, viewProfile);
 export default profileRouter;
