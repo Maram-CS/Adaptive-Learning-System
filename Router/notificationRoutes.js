7348b6d0e95548f3da83c51e0e034a4be1661721
Belkhairi Lyna Anfel <lynablk.dev@gmail.com>
2026-04-22 00:17:28 +0100

    modify notifications correct version without date

--- Router/notificationRoutes.js
+++ Router/notificationRoutes.js
@@ -4,7 +4,7 @@ import Notification from "../Model/notificationModel.js";
 
 const router = Router();
 
-// GET all notifications
+// njib notf mn database w n3awd nhotha f json
 router.get("/api/notifications", authRequest, async (req, res) => {
     try {
         const notifications = await Notification.find({
@@ -14,6 +14,60 @@ router.get("/api/notifications", authRequest, async (req, res) => {
         res.json({
             success: true,
             notifications
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+// mark as read
+router.put("/api/notifications/:id/read", authRequest, async (req, res) => {
+    await Notification.findOneAndUpdate(
+        { _id: req.params.id, studentId: req.id },
+        { is_read: true }
+    );
+
+    res.json({ success: true });
+});
+
+//supprimer notification
+router.delete("/api/notifications/:id", authRequest, async (req, res) => {
+    await Notification.findOneAndDelete({
+        _id: req.params.id,
+        studentId: req.id
+    });
+
+    res.json({ success: true });
+});
+
+// get unread notifications count
+router.get("/api/notifications/unread-count", authRequest, async (req, res) => {
+    const count = await Notification.countDocuments({
+        studentId: req.id,
+        is_read: false
+    });
+
+    res.json({
+        success: true,
+        unreadCount: count
+    });
+});
+// mark all as read
+router.put("/api/notifications/read-all", authRequest, async (req, res) => {
+    try {
+
+        await Notification.updateMany(
+            { studentId: req.id, is_read: false },
+            { $set: { is_read: true } }
+        );
+
+        res.json({
+            success: true,
+            message: "All notifications marked as read"
         });
 
     } catch (err) {
