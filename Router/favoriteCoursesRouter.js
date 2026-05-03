9b84f77f87d44be41bb6365fadc8f5f186a7e969
Maram BERRAHAL <maramberrahal5@gmail.com>
2026-05-03 20:13:16 +0100

    here i fixed quiz css

--- Router/favoriteCoursesRouter.js
+++ Router/favoriteCoursesRouter.js
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import { Router } from "express";
 import { getAllCourses } from "../Controller/favoriteCourseController.js";
 
 const favoriteCoursesRouter = Router();
